1131628d667e32ddc7e8dbf7d33de99cf16f08ff
Ilya Gorbunov <daverk@gmail.com>
2022-09-12 15:29:35 +0300

    fix: Debugged problems from issue #2 and made some changes accordingly changed python3 path to system updated issw executable path updated README.md

--- fn.py
+++ fn.py
@@ -1,17 +1,15 @@
-#!/usr/bin/env python3
-
 import os
 from pynput import keyboard
 
 def on_press(key):
     key_str = '{0}'.format(key)
     if (key_str == '<179>'):
-        stream = os.popen('issw')
+        stream = os.popen('/usr/local/bin/issw')
         output = stream.read().strip()
-        if (output == 'com.apple.keylayout.ABC'):
-            os.system('issw com.apple.keylayout.Russian')
+        if (output == 'com.apple.keylayout.US'):
+            os.system('/usr/local/bin/issw com.apple.keylayout.Russian')
         else:
-            os.system('issw com.apple.keylayout.ABC')
+            os.system('/usr/local/bin/issw com.apple.keylayout.US')
 
 
 with keyboard.Listener(on_press=on_press, on_release=None) as listener:
