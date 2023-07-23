37648b6aa642ce061e81bba4d6629757bcc57dca
Saba <sabato.sabato@gmail.com>
2023-07-24 00:03:11 +0300

    Black

--- fn.py
+++ fn.py
@@ -1,15 +1,17 @@
 import os
+
 from pynput import keyboard
 
+
 def on_press(key):
-    key_str = '{0}'.format(key)
-    if (key_str == '<179>'):
-        stream = os.popen('/usr/local/bin/issw')
+    key_str = "{0}".format(key)
+    if key_str == "<179>":
+        stream = os.popen("/usr/local/bin/issw")
         output = stream.read().strip()
-        if (output == 'com.apple.keylayout.ABC'):
-            os.system('/usr/local/bin/issw com.apple.keylayout.Russian')
+        if output == "com.apple.keylayout.ABC":
+            os.system("/usr/local/bin/issw com.apple.keylayout.Russian")
         else:
-            os.system('/usr/local/bin/issw com.apple.keylayout.ABC')
+            os.system("/usr/local/bin/issw com.apple.keylayout.ABC")
 
 
 with keyboard.Listener(on_press=on_press, on_release=None) as listener:
