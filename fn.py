390e5efa17e292e337e0d4bc887428a65e78f465
Saba <sabato.sabato@gmail.com>
2023-07-24 01:23:10 +0300

    Cycle through inputs discovered at startup

--- fn.py
+++ fn.py
@@ -6,16 +6,20 @@ COM_APPLE_KEYLAYOUT = "com.apple.keylayout"
 ISSW = "/usr/local/bin/issw"
 
 
-def on_press(key):
-    # "<179>" is fn-key
-    if str(key) != "<179>":
-        return
-    stream = os.popen(ISSW)
-    output = stream.read().strip()
-    if output == "com.apple.keylayout.ABC":
-        os.system(f"{ISSW} com.apple.keylayout.Russian")
-    else:
-        os.system(f"{ISSW} com.apple.keylayout.ABC")
+def on_press(inputs: list[str]):
+    def handler(key):
+        # "<179>" is fn-key
+        if str(key) != "<179>":
+            return
+        stream = os.popen(ISSW)
+        output = stream.read().strip()
+        current_input_index = inputs.index(output)
+        next_index = (current_input_index + 1) % len(inputs)
+        next_input = inputs[next_index]
+        os.popen(f"{ISSW} {next_input}")
+
+    print(f"Current inputs: [{', '.join(inputs)}]")
+    return handler
 
 
 def list_inputs():
@@ -26,5 +30,7 @@ def list_inputs():
 
 
 if __name__ == "__main__":
-    with keyboard.Listener(on_press=on_press, on_release=None) as listener:
+    with keyboard.Listener(
+        on_press=on_press(list_inputs()), on_release=None
+    ) as listener:
         listener.join()
