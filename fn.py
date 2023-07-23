fd9445253823fa8f325b334b791e6245a696a84f
Saba <sabato.sabato@gmail.com>
2023-07-24 00:55:37 +0300

    ISSW Constant

--- fn.py
+++ fn.py
@@ -3,22 +3,23 @@ import os
 from pynput import keyboard
 
 COM_APPLE_KEYLAYOUT = "com.apple.keylayout"
+ISSW = "/usr/local/bin/issw"
 
 
 def on_press(key):
     # "<179>" is fn-key
     if str(key) != "<179>":
         return
-    stream = os.popen("/usr/local/bin/issw")
+    stream = os.popen(ISSW)
     output = stream.read().strip()
     if output == "com.apple.keylayout.ABC":
-        os.system("/usr/local/bin/issw com.apple.keylayout.Russian")
+        os.system(f"{ISSW} com.apple.keylayout.Russian")
     else:
-        os.system("/usr/local/bin/issw com.apple.keylayout.ABC")
+        os.system(f"{ISSW} com.apple.keylayout.ABC")
 
 
 def list_inputs():
-    stream = os.popen("/usr/local/bin/issw -l")
+    stream = os.popen(f"{ISSW} -l")
     outputs = stream.read().strip().split("\n")
     inputs = [line for line in outputs if line.startswith(COM_APPLE_KEYLAYOUT)]
     return inputs
